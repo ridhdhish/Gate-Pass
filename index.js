e53c8af613b5ece7c21e130b222f87b17c206eab
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-04-28 21:29:57 +0530

    Procfile

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = mongoose
   })
   .then(() => {
     console.log("Connected");
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
